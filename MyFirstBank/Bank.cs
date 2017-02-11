0439eee03eaaf273805c7d2845fc04dcd8e2be0a
himal_000 <himalipethe@gmail.com>
2017-02-10 16:04:34 -0800

    first commit

---
+++ MyFirstBank/Bank.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{     // if a class is staic every methiod, property should be static
+    public static class Bank //pascal case .. first letter of every word is up case as oppsed to camel case
+    {
+        //something that does not need to be seen is a variable as opposed to a property
+        private static List<Account> accounts = new List<Account>();
+
+
+        public static string Name { get; set; }
+
+        public static Account CreateAccount(string emailAdddress, decimal amount)
+        {
+            var account = new Account(emailAdddress, amount);
+            accounts.Add(account);
+            return account;
+        }
+
+        public static void PrintAllAccounts(string emailAddress)
+        {
+            //a is jsut a variable and can be called whatever you want
+            foreach (var account in accounts.Where(a => a.EmailAddress == emailAddress))
+            {
+                Console.WriteLine($"Account number: {account.AccountNumber}, Balance: {account.Balance:C}");
+            }
+        }
+    }
+}
---
+++ MyFirstBank/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+          {
+                Console.WriteLine("**Welcome to my Bank**");
+            Console.WriteLine("Please enter your email address");
+            var emailAddress = Console.ReadLine();
+            while (true)
+                {
+                    Console.WriteLine("0. Exit");
+                    Console.WriteLine("1. Create an account");
+                    Console.WriteLine("2. Deposit");
+                    Console.WriteLine("3. Withdraw");
+                Console.WriteLine("4. Print all accounts");
+                    Console.WriteLine("Please select one of the options above");
+                    var choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "0":
+                            return;
+                        case "1":
+                        Console.WriteLine("Please enter your email address");
+                        var emailAddress2 = Console.ReadLine();
+                        var myAccount = Bank.CreateAccount(emailAddress, 0.0M);
+                            Console.WriteLine($"The balance in my account - {myAccount.AccountNumber} is {myAccount.Balance:C} ");
+                            break;
+                        case "2":
+                            break;
+                        case "3":
+                            break;
+                       case "4":
+                        Bank.PrintAllAccounts(emailAddress);
+                         break;
+                        default:
+                            Console.WriteLine("Sorry option not available");
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+
+    
+
